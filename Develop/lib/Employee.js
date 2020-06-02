5e4434cc5ff424e13d172dbfcd1ccd9e27a6ef67
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-06-01 21:21:23 -0500

    set up the parent class with its' properties and methods

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,7 +1,34 @@
 // TODO: Write code to define and export the Employee class
 
+//parent class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email
+    };
+    getName() {
+        return this.name;
+    };
+    getId() {
+        return this.id;
+    };
+    gitEmail() {
+        return this.email;
+    };
+    getRole() {
+        return 'Employee'
+    };
+    //Returns 'Employee'
+}
 
 
+}
+
+
+// 1. Run tests
+// 2. Create or update classes to pass a single test case
+// 3. Repeat
 
 
 
